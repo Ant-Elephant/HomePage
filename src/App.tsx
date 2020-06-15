2ee0c77dabaa6909a1e4fe58dc30e5dce4eed8bc
jichaochen <brpoper@gmail.com>
2020-06-15 22:38:00 +0800

    test scm trigger

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Ant-Elephant
+            Ant-Elephant. DEVOPS WORKFLOW
         </p>
       </header>
     </div>
